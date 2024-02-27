67f77176ac94c89e95a39e47792fa35b71ec9536
Vedang Utekar <utekar.vedang2002@gmail.com>
2024-02-27 16:27:44 +0530

    usersearch add

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,23 +1,38 @@
 import React,{useState, useEffect} from 'react'
 import styled from 'styled-components'
 import Logo from '../assets/logo.svg'
+import axios from 'axios'
+import { getSavedChatsRoute } from '../utils/APIRoutes'
 
 
 function Contacts({contacts, currentUser, changeChat}) {
     const [currentUserName, setCurrentUserName] = useState(undefined)
     const [currentUserImage, setCurrentUserImage] = useState(undefined)
     const [selectedUser, setSelectedUser] = useState(undefined)
+    const [searchUser, setSearchUser] = useState('')
+    const [savedContacts, setSavedContacts] = useState([])
     useEffect(() => {
         if(currentUser){
             setCurrentUserName(currentUser.username)
             setCurrentUserImage(currentUser.avatarImage)
+            getSavedContacts();
         }
-    }, [currentUser]);
+    }, [currentUser, savedContacts]);
+
+    
 
     const changeCurrentChat = (index, contact) => {
         setSelectedUser(index)
         changeChat(contact)
         
+    }
+
+    const filteredContacts = contacts.filter(contact => 
+        contact.username.toLowerCase().includes(searchUser.toLowerCase()))
+
+    const getSavedContacts = async () => {
+        const data = await axios.post(getSavedChatsRoute, {from: currentUser._id})
+        setSavedContacts(data.data)
     }
 
   return <>
@@ -28,9 +43,23 @@ function Contacts({contacts, currentUser, changeChat}) {
                     <img src={Logo} alt="logo" />
                     <h3>jetchat</h3>
                 </div>
+                <input className="username-inp" type="text" placeholder="Search username..." value={searchUser} onChange={e => setSearchUser(e.target.value)}/>
                 <div className="contacts">
                     {
-                        contacts.map((contact, index) => {
+                        searchUser === '' ? 
+                        (
+                          savedContacts.map((contact, index) => {
+                            return (<div className={`contact ${index === selectedUser ? 'selected' : ''}`} key={index} onClick={() => changeCurrentChat(index, contact)}>
+                                <div className="avatar">
+                                    <img src={`data:image/svg+xml;base64,${contact.avatarImage}`} alt="avatar" /> 
+                                </div>
+                                <div className="username">
+                                    <h3>{contact.username}</h3>
+                                </div>
+                            </div>)
+                          })
+                        ) : (
+                        filteredContacts.map((contact, index) => {
                             return (<div className={`contact ${index === selectedUser ? 'selected' : ''}`} key={index} onClick={() => changeCurrentChat(index, contact)}>
                                 <div className="avatar">
                                     <img src={`data:image/svg+xml;base64,${contact.avatarImage}`} alt="avatar" /> 
@@ -40,6 +69,7 @@ function Contacts({contacts, currentUser, changeChat}) {
                                 </div>
                             </div>)
                         })
+                        )
                     }
                 </div>
                 <div className="currentuser">
@@ -60,7 +90,7 @@ function Contacts({contacts, currentUser, changeChat}) {
 
 const Container = styled.div`
     display: grid;
-    grid-template-rows: 10% 75% 15%;
+    grid-template-rows: 10% 10% 65% 15%;
     overflow: hidden;
     background-color: #080420;
     .brand {
@@ -75,6 +105,16 @@ const Container = styled.div`
             color: white;
             text-transform: uppercase;
         }
+    }
+    .username-inp {
+        background-color: #ffffff39;
+        border: none;
+        outline: none;
+        padding: 0.5rem;
+        color: white;
+        font-size: 1.2rem;
+        text-align: center;
+        border-radius: 0.2rem;
     }
     .contacts {
         display: flex;
@@ -146,6 +186,9 @@ const Container = styled.div`
 
                 font-size: 1rem;
             }
+    }
+    @media screen and (max-width: 720px) {
+        
     }
 
 `;
